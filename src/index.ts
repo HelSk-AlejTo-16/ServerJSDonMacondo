3f009e73b402bd8a64e56b434ae1405b520607e4
LEONEL TORRES <leyotorres1501@gmail.com>
2024-11-28 11:45:00 -0600

    Chamba en JS

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Server from "./models/server";
-import dontenv from 'dotenv';
-
-//Configuracion de valores de ambiente (env)
-dontenv.config();
-
-const server = new Server();
--- src/routes/usuario.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { loginUser } from '../controllers/usuario';
-
-const routerLogin = Router();
-
-routerLogin.post('/', loginUser);
-
-export default routerLogin;
