0c38666501974da60085cac126528655b38fbd7d
LEONEL TORRES <leyotorres1501@gmail.com>
2024-11-29 21:52:21 -0600

    ErrorRaro3

--- models/server.js
+++ models/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./../db/connection");
-/*var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -15,8 +15,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
+
 const sucursal_1 = __importDefault(require("../routes/sucursal"));
 const connection_1 = __importDefault(require("../db/connection"));
 const producto_1 = __importDefault(require("../routes/producto"));
@@ -30,19 +29,8 @@ const distribuidors_1 = __importDefault(require("../routes/distribuidors"));
 const notas_1 = __importDefault(require("../routes/notas"));
 const usuario_1 = __importDefault(require("../routes/usuario"));
 const cliente_1 = __importDefault(require("../routes/cliente"));
-*/
-const sucursalRoutes = require("../routes/sucursal");
-const productoRoutes = require("../routes/producto");
-const ventaRoutes = require("../routes/venta");
-const juegoRoutes = require("../routes/juego");
-const empleadoRoutes = require("../routes/empleado");
-const rolRoutes = require("../routes/rol");
-const datoRoutes = require("../routes/dato");
-const tipProdsRoutes = require("../routes/tip_prods");
-const distribuidorsRoutes = require("../routes/distribuidors");
-const notasRoutes = require("../routes/notas");
-const usuarioRoutes = require("../routes/usuario");
-const clienteRoutes = require("../routes/cliente");
+
+
 
 
 
@@ -51,10 +39,13 @@ class Server {
         //this.app = (0, express_1.default)();
         this.app = express();
         this.port = process.env.PORT || '3001';
-        this.listen();
         this.midlewares();
         this.routes();
         this.dbConnect();
+        this.listen();
+        
+        
+        
        
     }
     listen() {
@@ -68,18 +59,18 @@ class Server {
                 msg: 'API Working'
             });
         });
-        this.app.use('/api/sucursales', sucursalRoutes);
-        this.app.use('/api/productos', productoRoutes);
-        this.app.use('/api/ventas',  ventaRoutes);
-        this.app.use('/api/juegos', juegoRoutes);
-        this.app.use('/api/empleados', empleadoRoutes);
-        this.app.use('/api/roles', rolRoutes);
-        this.app.use('/api/datos', datoRoutes);
-        this.app.use('/api/tip_Prod', tipProdsRoutes);
-        this.app.use('/api/distribuidors', distribuidorsRoutes);
-        this.app.use('/api/Notas', notasRoutes);
-        this.app.use('/api/login', usuarioRoutes);
-        this.app.use('/api/clientes', clienteRoutes);
+        this.app.use('/api/sucursales', sucursal_1.default);
+        this.app.use('/api/productos', producto_1.default);
+        this.app.use('/api/ventas',  venta_1.default);
+        this.app.use('/api/juegos', juego_1.default);
+        this.app.use('/api/empleados', empleado_1.default);
+        this.app.use('/api/roles', rol_1.default);
+        this.app.use('/api/datos', dato_1.default);
+        this.app.use('/api/tip_Prod', tip_prods_1.default);
+        this.app.use('/api/distribuidors', distribuidors_1.default);
+        this.app.use('/api/Notas', notas_1.default);
+        this.app.use('/api/login', usuario_1.default);
+        this.app.use('/api/clientes', cliente_1.default);
     }
     midlewares() {
         //Parseamos el body
