e69c2ba8207d2e2d62c2b570a85a884107a97113
LEONEL TORRES <leyotorres1501@gmail.com>
2024-11-29 20:13:43 -0600

    AaaAaBD

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,10 @@ const sequelize_1 = require("sequelize");
     dialect: 'mysql',
     logging: false
   }); */
-const sequelize = new sequelize_1.Sequelize('mr_macondo', 'root', '123456789', {
+const sequelize = new sequelize_1.Sequelize('mr-macondo', 'admin', '123456789', {
+  host: 'mr-macondo.c74w8ysa6r4n.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
+    port: 3306,
     logging: false
 });
 exports.default = sequelize;
